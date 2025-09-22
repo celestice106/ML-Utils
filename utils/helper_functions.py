6a65fc2b2df2792849586d699f7499b9565076c0
Celestice <celestice106@gmail.com>
2025-09-22 19:33:23 +0700

    adding more setup

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -169,8 +169,15 @@ def plot_loss_curves(results):
 from typing import List
 import torchvision
 
+import pathlib
+import os
+import zipfile
+import requests
 
-from typing import Union, List
+from PIL import Image
+
+from pathlib import Path
+from typing import Tuple, Dict, List
 import torch
 import torchvision
 import matplotlib.pyplot as plt
@@ -307,3 +314,28 @@ def download_data(source: str,
             os.remove(data_path / target_file)
     
     return image_path
+
+# Make function to find classes in a directory
+def find_classes(dir_path: str) -> Tuple[List[str], Dict[str, int]]:
+    """Finds the class folder names in a target directory.
+    
+    Assumes target directory is in standard image classification format.
+
+    Args:
+        directory (str): target directory to load classnames from.
+
+    Returns:
+        Tuple[List[str], Dict[str, int]]: (list_of_class_names, dict(class_name: idx...))
+    
+    Example:
+        find_classes("food_images/train")
+        >>> (["class_1", "class_2"], {"class_1": 0, ...})
+    """
+    # Get the class names from the target directory
+    classes = sorted(entry.name for entry in os.scandir(dir_path) if entry.is_dir())
+    
+    if not classes:
+        raise FileNotFoundError(f"Couldn't find any class folders in {dir_path}.")
+    # Create a mapping from class names to indices
+    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, class_to_idx
